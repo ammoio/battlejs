aa0b564165dd38b927daa5d44e8baab4c975fd7c
Ray Ma <rhm262@gmail.com>
2014-02-21 15:42:53 -0800

    Server skeleton

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-/**
+/*
  * Module dependencies.
  */
 
@@ -7,15 +7,19 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+<<<<<<< HEAD
 var mongoose = require('mongoose');
 var Models = require('./app/models');
+=======
+var dbHelpers = require('./dbHelpers');
+var loginHelpers = require('./loginHelpers');
+
+>>>>>>> Server skeleton
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -32,6 +36,51 @@ if ('development' == app.get('env')) {
 
 console.log(Models);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Magic happens on port ' + app.get('port'));
+app.get('/', function() {
+   res.sendfile(__dirname + '/public/index.html'); 
+});
+
+
+//a specific game room
+ap.get('/game/:gameId', function() {
+
+  //if this game room exists
+  if (games[gameId]) {
+    //games[gameId].players.push(socket);
+  } else {
+    res.send(404);
+  }
+
+});
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log('What happens on port ' + app.get('port') + " stays on port " + app.get('port'));
+});
+
+
+//socket io logic
+var io = require('socket.io').listen(server);
+var clients = [];
+var games = {};
+io.sockets.on('connection', function (socket) {
+  //save the session id
+  clients.push(socket.id, socket);
+
+  //when newGame is clicked
+  socket.on('newGame', function() {
+    //generate new game id
+    var gameId = "";
+
+
+    return gameId;
+  });
+  
+  socket.on('ready', function(data) {
+    
+   });
+
+  socket.on('submit', function(data) {
+
+  });
 });
