0b7d965c0d39eb06d0099e69f6b11a18faf7c827
Alex Taylor <alexmckenley@gmail.com>
2014-02-21 22:17:47 -0800

    Implemented submit and submitResult socket endpoints

--- app/testHelpers.js
+++ app/testHelpers.js
@@ -23,38 +23,48 @@ module.exports = {
 
   validate: function(functionName, input){
     var d = Q.defer();
+    console.log(functionName);
 
     Models.Challenge.findOneQ({functionName: functionName})
     .then(function(challenge){
+      console.log("challenge", challenge);
       var success = true;
-
+      var count = 0;
+    
       challenge.inputs.forEach(function(input, index){
         var s = new Sandbox();
-        s.run(input + "\n" + functionName + "(" + challenge.inputs[i] + ");", function(output){
+        s.run(input + "\n" + functionName + "(" + input + ");", function(output){
           if(output.result.indexOf('Error') !== -1){
+            console.log("error in output");
             d.reject(output);
             success = false;
+            count += 1;
             return;
           }
+
           if(output.result !== challenge.outputs[index]){
             d.reject("Failed on input: " + input + ". " + "Expected " + output.result + " to equal " + challenge.outputs[index]);
             success = false;
+            count += 1;
             return;
           }
+
+          if(count === challenge.inputs.length){
+            if(success){
+              d.resolve("Passed " + challenge.inputs.length + " tests!");
+            } else {
+              d.reject("something went wrong");
+            }
+          }
+
         });
       });
 
-      if(success){
-        d.resolve("Passed " + challenge.inputs.length + " tests!");
-      }
-
     })
     .fail(function(err){
+      console.log("failed" + err);
       d.reject(err);
-    })
-
-
-
+    });
 
     return d.promise;
   }
--- test/sandbox.js
+++ test/sandbox.js
@@ -1,6 +1,31 @@
 var Sandbox = require('sandbox');
+var testHelpers = require('../app/testHelpers');
 
-var s = new Sandbox();
-s.run('function test (n) { console.log(n); return n;}; test(3);', function(output){
-  console.log(output);
+var mongoose = require('mongoose-q')();
+
+// connect to MongoDB
+mongoose.connect('mongodb://localhost/battlejs');
+
+var js = 'var reverseString = function (n) { console.log(n); return n;}; test(3);';
+
+// var s = new Sandbox();
+// s.run(js, function(output){
+//   console.log(output);
+// });
+
+testHelpers.run(js).then(function(output){
+  console.log("run output: " + output);
+})
+.fail(function(output){
+  console.log("run failure: ", + output);
 });
+
+testHelpers.validate("reverseString", js)
+.then(function(output){
+  console.log("validate success: " + output);
+})
+.fail(function(output){
+  console.log("validate failure: " +  output.result);
+});
+
+
