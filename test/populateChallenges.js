b287aefd34f1fca9b7626bacf54cf5d2d41cb278
Alex Taylor <alexmckenley@gmail.com>
2014-02-21 18:42:42 -0800

    added populateChallenges.js

---
+++ test/populateChallenges.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose-q')();
+var Models = require('../app/models');
+
+// connect to MongoDB
+mongoose.connect('mongodb://localhost/battlejs');
+
+var reverseString = new Models.Challenge({
+  name: "Reverse String",
+  inputs: ['hello', 'a', 'get it together', '{{`12})3'],
+  outputs: ["olleh","a","rehtegot ti teg","3)}21`{{"],
+  solution: 'var reverseString = function (input){return input.split("").reverse().join("");};',
+  functionName: "reverseString",
+  boilerplate: "var reverseString = function (string) {\n  return string;\n};"
+});
+reverseString.saveQ()
+.then(function(){
+  console.log("saved Successfully");
+});
+
