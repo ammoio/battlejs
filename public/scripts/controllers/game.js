75a25cab7495c737724df0bd36e58241f83174ad
Daniel Almaguer <danielalmaguer@gmail.com>
2014-02-21 19:50:31 -0800

    3rd argument for watcher

--- public/scripts/controllers/game.js
+++ public/scripts/controllers/game.js
@@ -39,7 +39,7 @@ angular.module('app')
       };
 
       $scope.runCode = function() {
-        $rootScope.socket.emit('test', { data: player.getValue(), gameID: gameID });
+        $rootScope.socket.emit('test', { data: player.getValue(), gameID: gameID, player: $rootScope.playerOne });
       };
 
       $scope.increaseFont = function() {
@@ -86,7 +86,8 @@ angular.module('app')
       }
 
       $rootScope.socket.on('gameReady', function(response){
-        player.setValue(response.data, 1);
+        //response.name = name of the problem
+        player.setValue(response.biolerplate, 1);
       });
       
       $rootScope.socket.on('gameFull', function(data){
@@ -98,6 +99,6 @@ angular.module('app')
       });
 
       $rootScope.socket.on('testResults', function(obj) {
-        console.log(obj.console);
+        console.log(obj);
       });
   });
