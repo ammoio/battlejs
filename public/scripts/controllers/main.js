e43cdc5ef0c4e871dfa2709e8e3e8730528afa5e
Will Gester <willgester@gmail.com>
2014-02-22 22:28:03 -0800

    Added basic join functionality, watch bug

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -3,22 +3,33 @@ angular.module('app')
   .controller('MainController',
     function($scope, $location, $rootScope, $timeout) {
 
-
       $scope.welcome = "BattleJS";
 
 
       $scope.newGame = function(){
-        $rootScope.socket.emit('newGame', {});
+        $rootScope.socket.emit('newGame', {newGame: $scope.wantsNewGame});
       };
-      $rootScope.socket.on('gameID', function(data){
 
+      $rootScope.socket.on('gameID', function(data){
         var gameID = data.gameID;
 
-        $rootScope.gameID = gameID;
-        //Flag for first player
-        $rootScope.playerOne = true;
-        //Handles angular digest cycle
-        $timeout(function(){$location.path('/game/' + gameID);}, 0);
+        if ($scope.wantsNewGame){
+
+          $rootScope.gameID = gameID;
+          //Flag for first player
+          $rootScope.playerOne = true;
+          //Handles angular digest cycle
+          $timeout(function(){$location.path('/game/' + gameID);}, 0);
+        } else {
+          $timeout(function(){$location.path('/game/' + gameID);}, 0);
+        }
 
       });
+
+      $scope.new = function(){
+        $scope.wantsNewGame = true;
+      };
+      $scope.join = function(){
+        $scope.wantsNewGame = false;
+      };
   });
